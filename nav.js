5405ea5256baf56eedf7ec33b5212209c73f93dc
alinagoal <alidangol71@gmail.com>
2022-02-14 15:55:44 +0000

    add: json data as a response

--- nav.js
+++ nav.js
@@ -4,7 +4,9 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) =>{
-    res.send("welcome to my home page")
+    res.write("<h1>welcome to my home page</h1>")
+    res.write("<h1>welcome to my home page again</h1>")  //multiple response
+    res.send();  // closes the connection
 })
 
 app.get('/about', (req, res) =>{
@@ -15,8 +17,15 @@ app.get('/contact', (req, res) =>{
     res.send(" welcome to my contact page")
 })
 
-app.get('/temp', (req, res) =>{
-    res.send(" welcome to my temp page")
+app.get('/temp', (req, res) => {
+    res.send([{
+        id: 1,
+        name: "alina",
+    },
+    {
+        id: 1,
+        name: "alina",
+    }])
 })
 
 app.listen(port, () =>{
